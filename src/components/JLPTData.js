369625d5bc731a13d6341a378b2d3f660e466cb4
Dylan Brain <dylanbrain@googlemail.com>
2021-10-15 16:04:24 +0900

    N5 sample data added

---
+++ src/components/JLPTData.js
@@ -0,0 +1,37 @@
+const dataArray = [];
+
+const N5Array = [
+  {
+    name: "浴びる",
+    dictionaryForm: "あびる",
+    pastForm: "あびた",
+  },
+  {
+    name: "上げる",
+    dictionaryForm: "あげる",
+    pastForm: "あげた",
+  },
+  {
+    name: "開ける",
+    dictionaryForm: "あける",
+    pastForm: "あけた",
+  },
+  {
+    name: "開く",
+    dictionaryForm: "あく",
+    pastForm: "開けた",
+  },
+  {
+    name: "洗う",
+    dictionaryForm: "あらう",
+    pastForm: "あらった",
+  },
+];
+
+const N4Array = [];
+
+const N3Array = [];
+
+const N2Array = [];
+
+const N1Array = [];
