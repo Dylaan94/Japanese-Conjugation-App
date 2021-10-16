e268743c7ad2dc9105843fc041e540c5375d880a
Dylan Brain <dylanbrain@googlemail.com>
2021-10-16 14:35:10 +0900

    state updates based on selection (only for N5 and N4)

--- src/components/Controller.js
+++ src/components/Controller.js
@@ -1,14 +1,27 @@
 import React, { Component } from "react";
+import JLPTData from "./JLPTData";
 
 class Controller extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      N5: JLPTData.N5,
+      N4: JLPTData.N4,
+      N3: JLPTData.N3,
+      N2: JLPTData.N2,
+      N1: JLPTData.N1,
+    };
+
+  }
+
   render() {
     return (
       <div>
         <div>
-          <h1> Verb Displayed here</h1>
+          <h1> {this.state.N5[0].name}</h1>
         </div>
         <div>
-            <input type="text"/> 
+          <input type="text" />
         </div>
       </div>
     );
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,47 +1,68 @@
 import React, { Component } from "react";
-import Header from "./Header"
-import Footer from "./Footer"
-import GrammarPoint from "./GrammarPoint"
-import Controller from "./Controller"
-import Levels from "./Levels"
+import Header from "./Header";
+import Footer from "./Footer";
+import GrammarPoint from "./GrammarPoint";
+import Controller from "./Controller";
+import Levels from "./Levels";
 
-import JLPTData from "./JLPTData"
+import JLPTData from "./JLPTData";
 
 class Main extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            N5: JLPTData.N5,
-            N4: JLPTData.N4,
-            N3: JLPTData.N3,
-            N2: JLPTData.N2,
-            N1: JLPTData.N1
-        }
-        this.handleLevelsInput = this.handleLevelsInput.bind(this)
-    }
-    handleLevelsInput(data) {
-        let levelChosen = data
-        console.log("dealing with levels" + levelChosen)
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      N5: JLPTData.N5,
+      N4: JLPTData.N4,
+      N3: JLPTData.N3,
+      N2: JLPTData.N2,
+      N1: JLPTData.N1,
+    };
+    this.handleLevelsInput = this.handleLevelsInput.bind(this);
+  }
+  handleLevelsInput(data) {
+    let levelChosen = data;
+    let selectedLevelArray = [];
+    console.log("dealing with levels" + levelChosen);
 
+    if (levelChosen == "N5") {
+      selectedLevelArray = this.state.N5;
+      this.setState(
+        {
+          selected: selectedLevelArray,
+        },
+        () => {
+          console.log(this.state);
+        }
+      ); //setState is async so have to wait
+    } else if (levelChosen == "N4") {
+        selectedLevelArray = this.state.N4;
+        this.setState({
+            selected:selectedLevelArray
+        }, () => {
+                console.log(this.state)
+        })
+    }
+  }
 
   render() {
-      return (
-        <div>
-          <Header></Header>
-          <Levels handleLevelsChange={this.handleLevelsInput}></Levels>
-          <GrammarPoint></GrammarPoint>
-          <div>
+    return (
+      <div>
+        <Header></Header>
+        <Levels handleLevelsChange={this.handleLevelsInput}></Levels>
+        <GrammarPoint></GrammarPoint>
+        <Controller></Controller>
+
+        {/* <div>
             <div>
                       <h1> {this.state.N5[0].name} </h1>
             </div>
             <div>
               <input type="text" />
             </div>
-          </div>
-          <Footer></Footer>
-        </div>
-      );
+          </div> */}
+        <Footer></Footer>
+      </div>
+    );
   }
 }
 
