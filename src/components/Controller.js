532c30699d4feb63fe0bfea714b80d23319371c6
Dylan Brain <dylanbrain@googlemail.com>
2021-11-04 17:58:15 +0900

    added wanakana

--- src/components/Controller.js
+++ src/components/Controller.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
+import * as wanakana from "wanakana";
 
-import Styles from "./styles/Styles"
+import Styles from "./styles/Styles";
 
 class Controller extends Component {
+  componentDidMount() {
+    let textInput = document.getElementById("textInput");
+    wanakana.bind(textInput);
+  }
+
   render() {
     const {
       randomisedValue,
@@ -10,26 +16,27 @@ class Controller extends Component {
       handleTextInput,
       checkTextInput,
     } = this.props; // destructure props
-      return (
-        <Styles.ControllerMain>
-            <div className = "textDisplayDiv">
-              <h1> {randomisedValue.name} </h1>
-              <h2> {randomisedValue.verbType}</h2>
-            </div>
-            <div className = "inputDiv">
-              <input
-                type="text"
-                      value={value}
-                      placeholder= "type here"
-                onChange={(text) => {
-                  handleTextInput(text);
-                  checkTextInput();
-                }}
-                name="textInputValue"
-              />
-            </div>
-        </Styles.ControllerMain>
-      );
+    return (
+      <Styles.ControllerMain>
+        <div className="textDisplayDiv">
+          <h1> {randomisedValue.name} </h1>
+          <h2> {randomisedValue.verbType}</h2>
+        </div>
+        <div className="inputDiv">
+          <input
+            id="textInput"
+            type="text"
+            value={value}
+            placeholder="type here"
+            onChange={(text) => {
+              handleTextInput(text);
+              checkTextInput();
+            }}
+            name="textInputValue"
+          />
+        </div>
+      </Styles.ControllerMain>
+    );
   }
 }
 
