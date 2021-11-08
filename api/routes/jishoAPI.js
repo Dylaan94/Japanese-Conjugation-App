c50b8247ebaa48fca3b5e4d20235951ad9aace14
Dylan Brain <dylanbrain@googlemail.com>
2021-11-08 21:54:19 +0900

    cleaned up jishoAPI file

--- api/routes/jishoAPI.js
+++ api/routes/jishoAPI.js
@@ -1,10 +1,6 @@
 var express = require("express");
 var router = express.Router();
-var got = require("got");
 var axios = require("axios")
-
-var JishoAPI = require("unofficial-jisho-api");
-var jisho = new JishoAPI();
 
 // jisho.searchForPhrase("たべる").then((result) => {
 //     console.log(result.data[0].senses);
@@ -22,24 +18,6 @@ router.get('/', (req, res) => {
     });
 })
 
-// router.get("/", function (req, res, next) {
-//   const apiCall = async () => {
-//     try {
-//       const response = await got(
-//         "https://jisho.org/api/v1/search/words?keyword=%23jlpt-n5&page=2"
-//       );
-//       console.log(response.body);
-//     } catch (error) {
-//       console.log(error.response.body);
-//     }
-//   };
-// });
 
-// jisho.searchForPhrase("#jlpt-n5 #verb").then((result) => {
-//   console.log(result);
-//   router.get("/", function (req, res, next) {
-//     res.send(JSON.stringify(result));
-//   });
-// });
 
 module.exports = router;
