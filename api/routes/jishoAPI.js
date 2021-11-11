92355d2522403c155e434efe4fef555cc3c6ce86
Dylan Brain <dylanbrain@googlemail.com>
2021-11-11 20:11:26 +0900

    all n5 data returned by API call

--- api/routes/jishoAPI.js
+++ api/routes/jishoAPI.js
@@ -1,39 +1,45 @@
-var express = require("express");
-var router = express.Router();
-var axios = require("axios");
+let express = require("express");
+let router = express.Router();
+let axios = require("axios");
 
-// jisho.searchForPhrase("たべる").then((result) => {
-//     console.log(result.data[0].senses);
-//     router.get("/", function (req, res, next) {
-//       res.send(JSON.stringify(result));
-//     });
-// });
-
-var n5Array = [];
+let n5Array = [];
+let promises = [];
+let callN5Verbs = function () {};
 
 // need to run a number of requests based on the number of pages.
+let pages = 7;
+
 router.get("/", (req, res) => {
-  axios
-    .get(
-      "https://jisho.org/api/v1/search/words?keyword=%23verb%20%23jlpt-n5&page=2" // this selects all verbs on page 2
-    )
-    .then((response) => {
-      for (let i = 0; i < response.data.data.length; i++) {
-        var verbObject = {
-          name: response.data.data[i].japanese[0].word,
-          english: response.data.data[i].senses[0].english_definitions[0],
-          dictionaryForm: response.data.data[i].japanese[0].reading,
-          verbType: response.data.data[i].senses[0].parts_of_speech[0],
-        };
-        n5Array.push(verbObject);
-      }
+  for (let i = 1; i < pages; i++) {
+    let pageNumber = i;
+    promises.push(
+      axios
+        .get(
+          "https://jisho.org/api/v1/search/words?keyword=%23verb%20%23jlpt-n5&page=" +
+            pageNumber +
+            "" // this selects all verbs on pageNumber
+        )
+        .then((response) => {
+          for (let i = 0; i < response.data.data.length; i++) {
+            let verbObject = {
+              name: response.data.data[i].japanese[0].word,
+              english: response.data.data[i].senses[0].english_definitions[0],
+              dictionaryForm: response.data.data[i].japanese[0].reading,
+              verbType: response.data.data[i].senses[0].parts_of_speech[0],
+            };
+            n5Array.push(verbObject);
+          }
 
-      console.log(response.data.data);
+          console.log(response.data.data);
 
-      var data = response.data.data;
-
-      res.send(n5Array);
-    });
+          let data = response.data.data;
+          console.log(n5Array);
+          //res.send(n5Array);
+        })
+    );
+  }
 });
+
+Promise.all(promises).then(() => console.log(n5Array)); // need to put this inside the loop??
 
 module.exports = router;
